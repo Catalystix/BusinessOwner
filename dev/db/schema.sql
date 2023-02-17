0ed26e6f6e5ec3dacbe34ee1bf33d42bac0e3128
Catalystix <dcode.green@gmail.com>
2023-02-16 20:26:50 -0500

    updated schema

--- dev/db/schema.sql
+++ dev/db/schema.sql
@@ -3,22 +3,26 @@ CREATE DATABASE emp_db;
 
 USE emp_db;
 
+
+CREATE TABLE department (
+  id AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30)
+);
+
 CREATE TABLE employee (
-  emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  job_title VARCHAR(30) NOT NULL,
-  department VARCHAR(30) NOT NULL,
-  salary INT NOT NULL,
-  reporting_manager VARCHAR(30) NOT NULL
+  role_id INT,
+  manager_id INT
 );
 
 
 CREATE TABLE role (
-  job_title VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  department VARCHAR(30) NOT NULL,
-  salary INT NOT NULL
+  ID INT PRIMARY KEY,
+  title VARCHAR(30),
+  department_id INT,
+  salary DECIMAL
 );
 
 --look into making database more like this from instructional #15
@@ -37,26 +41,5 @@ CREATE TABLE courses (
   date_added DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE students (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
 
-CREATE TABLE instructors (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
 
-CREATE TABLE classrooms (
-  id INT NOT NULL,
-  building_name VARCHAR(30) NOT NULL,
-  room_number INT NOT NULL,
-  available BOOLEAN NOT NULL,
-  date_updated DATETIME NOT NULL
-);
